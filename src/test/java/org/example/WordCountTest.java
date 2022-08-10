89da179223bc1399489d8809d28529275527129a
Erdem Sevilmis <51912221+Erdem-Sevilmis@users.noreply.github.com>
2022-08-10 11:17:24 +0200

    Added Tests

--- src/test/java/org/example/WordCountTest.java
+++ src/test/java/org/example/WordCountTest.java
@@ -1,25 +1,76 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordCountTest {
-    @Test
-    void getWordCountlvl1() {
-        WordCount wc = new WordCount("Heey my name is Erdem");
-        assertEquals(5, wc.getWordCount());
+    @ParameterizedTest
+    @ValueSource(strings = {"Word"})
+    void getWordWithSingleWord(String phrase) {
+        WordCount wc = new WordCount(phrase);
+        assertEquals(1, wc.getWordCount());
     }
 
-    @Test
-    void getWordCountlvl2() {
-        WordCount wc = new WordCount("Mary had a little lamb");
-        assertEquals(4, wc.getWordCount());
+    @ParameterizedTest
+    @ValueSource(strings = {"Word word word"})
+    void getWordWithMultipleWords(String phrase) {
+        WordCount wc = new WordCount(phrase);
+        assertEquals(3, wc.getWordCount());
     }
 
-    @Test
-    void readFromFile() {
-        var results = WordCount.readFile("src/main/resources/test.txt");
+    @ParameterizedTest
+    @ValueSource(strings = {"123 123 word 123", "1Word 2word word 3word", "Word1 word2 word3 word", "Word w1ord wo2rd word3"})
+    void getWordWithNumbers(String phrase) {
+        WordCount wc = new WordCount(phrase);
+        assertEquals(1, wc.getWordCount());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Word word word       ","        Word word word", "Word      word     word     "})
+    void getWordWithWhitespaces(String phrase) {
+        WordCount wc = new WordCount(phrase);
+        assertEquals(3, wc.getWordCount());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123    123 word   123", "1Word     2word word    3word     ", "   Word1 word2    word3 word", "Word   w1ord    wo2rd word3    "})
+    void getWordWithWhitespacesAndNumbers(String phrase) {
+        WordCount wc = new WordCount(phrase);
+        assertEquals(1, wc.getWordCount());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Word. word& word", "!Word word word$", "word W§§ord", "W$ord §$ word"})
+    void getWordWithSpecialCharacters(String phrase) {
+        WordCount wc = new WordCount(phrase);
+        assertEquals(1, wc.getWordCount());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"     Wo2rd.     wod& word    ", "   !Word word word4", "   wo1r§d    word W§§ord         ", "W$ord         §$ word         "})
+    void getWordWithSpecialCharactersAndWhitespacesAndNumbers(String phrase) {
+        WordCount wc = new WordCount(phrase);
+        assertEquals(1, wc.getWordCount());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"src/main/resources/test.txt"})
+    void readFromFile(String path) {
+        var results = WordCount.readFile(path);
         assertEquals(5, results.size());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ASD/ASD/"})
+    void readFromFileWithInvalidPath(String path) {
+        assertThrows(RuntimeException.class, () -> WordCount.readFile(path));
+    }
+
+    @Test
+    void readFromFileWithNULLPath() {
+        assertThrows(NullPointerException.class, () -> WordCount.readFile(null));
     }
 }
